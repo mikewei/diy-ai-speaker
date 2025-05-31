27b20ca4c28ec4cf8ef10d78d855680eea2735e1
mikewei <bin@vip.qq.com>
2025-06-01 02:28:37 +0800

    init

---
+++ start_vllm.sh
@@ -0,0 +1,10 @@
+python \
+  -m vllm.entrypoints.openai.api_server \
+  --model ~/data/llm-models/qwen3/ \
+  --served-model-name qwen3-14b-diy \
+  --gpu-memory-utilization 0.55 \
+  --max-model-len 4096 \
+  --max-num-seqs 2 \
+  --host 0.0.0.0 \
+  --port 8000
+
