5dd9cbae7dfb43cd84a86c036d3475344c70195f
mikewei <bin@vip.qq.com>
2025-06-05 00:24:34 +0800

    speaker echo done

--- speaker_svc.py
+++ speaker_svc.py
@@ -1,6 +1,7 @@
 import os
 import time
 import queue
+import threading
 import sounddevice as sd
 import soundfile as sf
 import torch
@@ -8,13 +9,15 @@ import numpy as np
 from pyannote.audio import Pipeline
 from kokoro import KPipeline
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
-from typing import Optional, NamedTuple, Dict
+from typing import Optional, NamedTuple, Dict, Literal
+
 
 class Vad:
     def __init__(self, cache_dir='/dev/shm'):
         self.pipeline = Pipeline.from_pretrained("pyannote/voice-activity-detection",
                                                 use_auth_token=os.environ['HF_TOKEN_FG'])
         self.cache_dir = cache_dir
+
     def detect(self, audio_array) -> bool:
         audio_file = os.path.join(self.cache_dir, 'vad_callback.wav')
         sf.write(audio_file, audio_array, 16000)
@@ -22,6 +25,7 @@ class Vad:
         for speech in output.get_timeline().support():
             return True
         return False
+
 
 class S2T:
     def __init__(self, model_id='openai/whisper-large-v3-turbo', sample_rate=16000):
@@ -34,6 +38,7 @@ class S2T:
         )
         self.model.to(self.device)
         self.processor = AutoProcessor.from_pretrained(model_id)
+
     def transcribe(self, audio_array) -> Optional[Dict]:
         inputs = self.processor(
             audio_array,
@@ -51,38 +56,58 @@ class S2T:
         pred_text = self.processor.batch_decode(pred_ids, skip_special_tokens=True, decode_with_timestamps=False)
         return dict(text=pred_text[0], lang='en' if lang_ids[0] == 50259 else 'zh')
 
+
 class AudioPlayer:
+    class Msg(NamedTuple):
+        audio_array: np.ndarray
+        block_idx: int
+
     def __init__(self, duration, samplerate, channels=1):
         self.duration = duration
         self.sample_rate = samplerate
         self.block_size = int(self.sample_rate * self.duration)
         self.channels = channels
         self.playing_audio_q = queue.Queue()
+        self.q_reset_lock = threading.Lock()
         self.stream = sd.OutputStream(samplerate=self.sample_rate, channels=self.channels, blocksize=self.block_size,
                                       callback=self.feed_audio)
         self.stream.start()
+
     def cleanup(self):
         self.stream.stop()
         self.stream.close()
+
     def play(self, audio_array):
         """ audio_array is a numpy array of shape (n, 1) """
-        self.playing_audio_q.put(audio_array)
+        for idx in range((audio_array.shape[0] + self.block_size - 1) // self.block_size):
+            msg = self.Msg(audio_array, idx)
+            self.playing_audio_q.put(msg)
+
+    def stop(self):
+        with self.q_reset_lock:
+            self.playing_audio_q.queue.clear()
+
     def feed_audio(self, outdata, frames, time_info, status):
+        assert self.block_size == frames
         if status:
             print(status)
         try:
-            block = self.playing_audio_q.get_nowait()
-            print(f'frames: {frames}')
-            assert block.shape[0] <= frames
+            with self.q_reset_lock:
+                msg = self.playing_audio_q.get_nowait()
+            left_block_size = int(msg.audio_array.shape[0]) - int(msg.block_idx * self.block_size)
+            assert left_block_size > 0
+            print(f'start playing block {msg.block_idx}')
             # padding
-            if block.shape[0] < frames:
-                pad_len = frames - block.shape[0]
+            if left_block_size < self.block_size:
+                block = msg.audio_array[msg.block_idx * self.block_size :]
+                pad_len = self.block_size - left_block_size
                 padded = np.pad(block, ((0, pad_len), (0, 0)), mode='constant')
                 outdata[:] = padded
             else:
-                outdata[:] = block
+                outdata[:] = msg.audio_array[msg.block_idx * self.block_size : (msg.block_idx + 1) * self.block_size]
         except queue.Empty:
             outdata.fill(0)
+
 
 class AudioProcessor:
     def __init__(self, vad: Vad, s2t: S2T, player: AudioPlayer):
@@ -93,9 +118,11 @@ class AudioProcessor:
         self.vad_active_idx = None
         self.audio_buffer = None
         self.prev_audio_array = None
+
     def process(self, audio_array):
         """ audio_array is a numpy array of shape (n) """
-        if self.vad.detect(audio_array):
+        detected_audio = audio_array if self.prev_audio_array is None else np.concatenate([self.prev_audio_array, audio_array])
+        if self.vad.detect(detected_audio):
             print(f'[{self.last_block_idx}] vad detected')
             if self.audio_buffer is None:
                 self.audio_buffer = audio_array if self.prev_audio_array is None else np.concatenate([self.prev_audio_array, audio_array])
@@ -106,14 +133,17 @@ class AudioProcessor:
             assert self.audio_buffer is not None
             # append one more block
             self.audio_buffer = np.concatenate([self.audio_buffer, audio_array])
-            self.player.play(self.audio_buffer.reshape(-1, 1))
             result = self.s2t.transcribe(self.audio_buffer)
             if result is not None:
                 print(f'[{self.vad_active_idx}-{self.last_block_idx}] transcribing {result["text"]} ({result["lang"]})')
+                if result["lang"] == 'zh':
+                    self.player.stop()
+                    self.player.play(self.audio_buffer.reshape(-1, 1))
             self.audio_buffer = None
             self.vad_active_idx = None
         self.prev_audio_array = audio_array
         self.last_block_idx += 1
+
 
 def recording_loop(duration, samplerate, callback=None):
     recording_audio_q = queue.Queue()
@@ -138,12 +168,14 @@ def recording_loop(duration, samplerate, callback=None):
     finally:
         stream.stop()
 
+
 class Config(NamedTuple):
     sample_rate: int = 16000
     recording_block_duration: float = 0.5
-    playing_block_duration: float = 2
+    playing_block_duration: float = 0.5
     running_cache_dir: str = '/dev/shm'
     s2t_model_id: str = 'openai/whisper-large-v3-turbo'
+
 
 if __name__ == '__main__':
     print('Initializing ...', flush=True)
